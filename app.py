83defcea5a675b2be82252b78d2a44113b934105
sayandeveloper <samantasayan23@gmail.com>
2024-08-18 22:00:14 +0530

    allow cors

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, Path
+from fastapi.middleware.cors import CORSMiddleware
+
 from contentBasedFiltering import *
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def get_dashboard_data():
